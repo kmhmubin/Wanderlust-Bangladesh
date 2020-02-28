0c329d7b9b14175fbb2eb18e0d78d2743d642288
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-02-28 19:33:38 +0600

    destination and hotel add

--- website_files/booking.php
+++ website_files/booking.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- fav icon -->
-    <link rel="icon" href="assets/images/favicon-96x96.png" type="image/png" />
+    <link rel="icon" href="assets/img/favicon-96x96.png" type="image/png" />
     <script src="https://kit.fontawesome.com/a2e320ebe3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/hotel.css">
     <title>Booking</title>
 </head>
 
@@ -45,6 +46,125 @@
             </div>
         </div>
     </nav>
+
+    <!-- header section -->
+
+    <header id="d_title">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 id="destination">Bookings</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="hotels">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-4">
+                    <!-- cards -->
+                    <div class="card border border-primary">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/maple_leaf.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Western Plus Maple Leaf</h4>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <i class="fas fa-map-marker-alt"></i> Dhaka
+                                </div>
+                                <div class="col-sm-6">
+                                    <i class="fas fa-star"></i> 4-star hotel
+                                </div>
+                            </div>
+                            <div class="card-text pt-2 pb-2">
+                                Straightforward airport hotel with modern rooms & suites, plus a spa, a restaurant & a bar.
+                            </div>
+                            <!-- footer button -->
+                            <div class="row text-center">
+                                <a class="btn btn-outline-primary btn-lg btn-block" href="#" role="button">Check Availability</a>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <!-- cards -->
+                    <div class="card border border-primary">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/maple_leaf.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Amari Dhaka</h4>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <i class="fas fa-map-marker-alt"></i> Dhaka
+                                </div>
+                                <div class="col-sm-6">
+                                    <i class="fas fa-star"></i> 5-star hotel
+                                </div>
+                            </div>
+                            <div class="card-text pt-2 pb-2">
+                                Elegant high-rise hotel offering 2 bar & a restaurant, plus a rooftop pool, a spa & event space.
+                            </div>
+                            <!-- footer button -->
+                            <div class="row text-center">
+                                <a class="btn btn-outline-primary btn-lg btn-block" href="#" role="button">Check Availability</a>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+
+                <div class="col-md-4 mb-4">
+                    <!-- cards -->
+                    <div class="card border border-primary">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/intercontinental.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">InterContinental Dhaka</h4>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <i class="fas fa-map-marker-alt"></i> Dhaka
+                                </div>
+                                <div class="col-sm-6">
+                                    <i class="fas fa-star"></i> 5-star hotel
+                                </div>
+                            </div>
+                            <div class="card-text pt-2 pb-2">
+                                The InterContinental Dhaka is a prominent luxury hotel in Ramna in central Dhaka,the capital of Bangladesh.
+                            </div>
+                            <!-- footer button -->
+                            <div class="row text-center">
+                                <a class="btn btn-outline-primary btn-lg btn-block" href="#" role="button">Check Availability</a>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+            </div>
+    </section>
 
     <section class="ftr">
         <footer class="page-footer font-small blue-grey lighten-5">
--- website_files/dest.php
+++ website_files/dest.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- fav icon -->
-    <link rel="icon" href="assets/images/favicon-96x96.png" type="image/png" />
+    <link rel="icon" href="assets/img/favicon-96x96.png" type="image/png" />
     <script src="https://kit.fontawesome.com/a2e320ebe3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/nav.css">
@@ -49,42 +49,218 @@
     <!-- header section -->
 
     <header id="d_title">
-        <div class="dark-overlay">
-            <div class="container d_inner">
-                <div class="row">
-                    <div class="col">
-                        <h1 class="display-1">Destination</h1>
-                    </div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1 id="destination">Destination</h1>
                 </div>
             </div>
         </div>
     </header>
-    <section class="td">
+
+    <!-- destination cards section -->
+    <section class="td py-4">
         <div class="container">
-            <div class="card-deck">
-                <div class="card bg-primary">
-                    <div class="card-body text-center">
-                        <p class="card-text">Some text inside the first card</p>
+            <!-- rows -->
+            <div class="row">
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-warning">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/dhaka-city.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Dhaka</h4>
+                            <p class="card-text">
+                                Huge modern city with 17th-century old town, home to Mughal Places & modern parliament complex.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="dhaka.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
                     </div>
+                    <!-- col end -->
                 </div>
-                <div class="card bg-warning">
-                    <div class="card-body text-center">
-                        <p class="card-text">Some text inside the second card</p>
+
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-success">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/des2.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Sundarbans</h4>
+                            <p class="card-text">
+                                One of the world's natural wonders, this massive forest is also a tiger & ecological preserve.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="sundarbans.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
                     </div>
+                    <!-- col end -->
                 </div>
-                <div class="card bg-success">
-                    <div class="card-body text-center">
-                        <p class="card-text">Some text inside the third card</p>
+
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-primary">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/des4.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Cox's Bazar</h4>
+                            <p class="card-text">
+                                Cox's Bazar is a city, fishing port, tourism centre in southeastern Bangladesh.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="cox.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
                     </div>
+                    <!-- col end -->
                 </div>
-                <div class="card bg-danger">
-                    <div class="card-body text-center">
-                        <p class="card-text">Some text inside the fourth card</p>
-                    </div>
-                </div>
+                <!-- rows end -->
             </div>
+
+            <!-- rows -->
+            <div class="row">
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-warning">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/cg.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Chittagong</h4>
+                            <p class="card-text">
+                                Chittagong is large port city on the southeastern coast of Bangladesh.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="dhaka.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-success">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/sylhet.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">Sylhet</h4>
+                            <p class="card-text">
+                                Sylhet is a city in eastern Bangladesh, on the Surma River.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="sundarbans.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+
+                <div class="col mb-4">
+                    <!-- cards -->
+                    <div class="card border border-primary">
+                        <!--card image -->
+                        <div class="view overlay">
+                            <img src="assets/img/stmartin.jpg" alt="Card image cap" class="card-img-top">
+                            <a href="#!">
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!-- card content -->
+                        <div class="card-body">
+                            <h4 class="card-title">St. Martin's Island</h4>
+                            <p class="card-text">
+                                St. Martin's Island is a small island in the northeastern part of the Bangladesh.
+                            </p>
+                            <!-- footer button -->
+                            <div class="row text-center" id="cf">
+                                <div class="col pl-3">
+                                    <a href="cox.php" class="btn btn-outline-info btn-compass"><i class="fas fa-compass"></i> Travel Guide</a>
+                                </div>
+                                <div class="col">
+                                    <a href="booking.php" class="btn btn-outline-info btn-hotel "><i class="fas fa-hotel"></i> Hotels</a>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- cards end -->
+                    </div>
+                    <!-- col end -->
+                </div>
+                <!-- rows end -->
+            </div>
+
+
         </div>
     </section>
+
+
+
 
     <section class="ftr">
         <footer class="page-footer font-small blue-grey lighten-5">
