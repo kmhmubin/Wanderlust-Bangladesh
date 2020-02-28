f4494101958d2ba3f3aee81a3ed9616f8ef84232
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-02-28 23:26:07 +0600

    dhaka added

---
+++ website_files/dhaka.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- fav icon -->
+    <link rel="icon" href="assets/img/favicon-96x96.png" type="image/png" />
+    <script src="https://kit.fontawesome.com/a2e320ebe3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/nav.css">
+    <link rel="stylesheet" href="assets/css/dhaka.css">
+    <title>Dhaka</title>
+</head>
+
+<body>
+    <!-- nav bar -->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark" id="main-nav">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">
+                <h3 id="name">Wanderlust Bangladesh</h3>
+            </a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="dest.php" class="nav-link">Explore</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="booking.php" class="nav-link">Booking</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="video.php" class="nav-link">Video</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="signin.php" class="nav-link">Log In</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="signup.php" class="nav-link btn btn-outline-info">Sign Up</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header id="dk_header">
+        <div class="container">
+            <h1>Dhaka</h1>
+            <h6>Capital of Bangladesh</h6>
+            <p pt-1>
+                Dhaka is the capital city of Bangladesh, in southern Asia. Set beside the Buriganga River, it’s at the center of national government, trade and culture. The 17th-century old city was the Mughal capital of Bengal, and many palaces and mosques remain. American architect Louis Khan’s National Parliament House complex typifies the huge, fast-growing modern metropolis.<br /><br />
+                In Dhaka’s old city, grand, 17th-century Lalbagh Fort contains a museum of paintings, weapons and decorative arts. The Dhakeshwari Temple is the focus of the city’s Hindu community. The National Museum traces Bangladesh’s natural, religious and political history. Graphic displays at the Liberation War Museum reveal the horrors of the 1971 War of Independence. The elaborate 19th-century Ahsan Manzil, or Pink Palace, stands beside the river. Wooden boats offer trips on the water from the busy Sadarghat boat terminal, the heart of traditional riverside life. Colorful painted rickshaws are the city’s main mode of transport.
+            </p>
+        </div>
+    </header>
+</body>
+
+</html>
