34d73948af695fb10afff72a6ad6402ee8b7c1c4
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-03-28 16:15:32 +0600

    include folder added

---
+++ admin/inc/footer.php
@@ -0,0 +1,9 @@
+    <!-- Footer -->
+    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright &copy; Wanderlust Bangladesh</span>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
