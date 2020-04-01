7f417d573031fcf2a6cdf6147f743ededf989d10
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-04-01 18:27:55 +0600

    Add security to Add admin and Categories
    Because:
    - without login user can't access the specific pages
    
    It's is used to give securiy for both user and the data leak

--- admin/add_admin.php
+++ admin/add_admin.php
@@ -143,3 +143,4 @@ if (isset($_POST['deleteBtn'])) {
         header("Location: admin_user.php");
     }
 }
+
--- admin/admin_edit.php
+++ admin/admin_edit.php
@@ -4,98 +4,120 @@ include "include/header.php";
 
 
 <!-- Sidebar -->
-<?php include "include/sidenavbar.php";  ?>
-<!-- End of Sidebar -->
-
-<!-- Content Wrapper -->
-<div id="content-wrapper" class="d-flex flex-column">
-
-    <!-- Main Content -->
-    <div id="content">
-
-        <!-- Topbar -->
-        <?php include "include/topnavbar.php";  ?>
-        <!-- End of Topbar -->
+<?php include "include/sidenavbar.php";
 
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+if (isset($_SESSION['username'])) {
+    // if username is true show those
+?>
 
-            <!-- Page Heading -->
-            <h1 class="h3 mb-2 text-gray-800">Edit Admin Profile</h1>
-            <div class="pt-5"></div>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+        <!-- Main Content -->
+        <div id="content">
+
+            <!-- Topbar -->
+            <?php include "include/topnavbar.php";  ?>
+            <!-- End of Topbar -->
 
 
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Edit Form</h6>
-                </div>
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
 
-                <div class="card-body">
+                <!-- Page Heading -->
+                <h1 class="h3 mb-2 text-gray-800">Edit Admin Profile</h1>
+                <div class="pt-5"></div>
 
 
-                    <?php
-                    if (isset($_POST['edit_btn'])) {
-                        $id = $_POST['edit_id'];
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Edit Form</h6>
+                    </div>
 
-                        $edit_data = "SELECT * FROM users WHERE id='$id'";
-                        $edit_response = mysqli_query($conn, $edit_data);
+                    <div class="card-body">
 
-                        foreach ($edit_response as $row) {
 
-                    ?>
-                            <form action="add_admin.php" method="POST">
-                                <!-- edit form -->
-                                <input type="hidden" name="edit_id" value="<?php echo $row['id']; ?>">
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" name="edit_fname" value="<?php echo $row['First_Name']; ?>" id="exampleFirstName" placeholder="First Name">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" name="edit_lname" value="<?php echo $row['Last_Name']; ?>" id="exampleLastName" placeholder="Last  Name">
-                                </div>
+                        <?php
+                        if (isset($_POST['edit_btn'])) {
+                            $id = $_POST['edit_id'];
 
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" name="edit_username" value="<?php echo $row['User_Name']; ?>" id="exampleInputUserName" placeholder="User Name">
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" name="edit_email" value="<?php echo $row['email']; ?>" id="exampleInputEmail" placeholder="Email Address">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user" name="edit_password" value="<?php echo $row['Password']; ?>" id="exampleInputPassword" placeholder="Password">
-                                </div>
+                            $edit_data = "SELECT * FROM users WHERE id='$id'";
+                            $edit_response = mysqli_query($conn, $edit_data);
 
-                                <a href="admin_user.php" class="btn btn-danger">Cancel</a>
-                                <button type="submit" name="updateBtn" class="btn btn-primary">Update</button>
-                            </form>
-                    <?php
+                            foreach ($edit_response as $row) {
 
+                        ?>
+                                <form action="add_admin.php" method="POST">
+                                    <!-- edit form -->
+                                    <input type="hidden" name="edit_id" value="<?php echo $row['id']; ?>">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" name="edit_fname" value="<?php echo $row['First_Name']; ?>" id="exampleFirstName" placeholder="First Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" name="edit_lname" value="<?php echo $row['Last_Name']; ?>" id="exampleLastName" placeholder="Last  Name">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" name="edit_username" value="<?php echo $row['User_Name']; ?>" id="exampleInputUserName" placeholder="User Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user" name="edit_email" value="<?php echo $row['email']; ?>" id="exampleInputEmail" placeholder="Email Address">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" name="edit_password" value="<?php echo $row['Password']; ?>" id="exampleInputPassword" placeholder="Password">
+                                    </div>
+
+                                    <a href="admin_user.php" class="btn btn-danger">Cancel</a>
+                                    <button type="submit" name="updateBtn" class="btn btn-primary">Update</button>
+                                </form>
+                        <?php
+
+                            }
                         }
-                    }
-                    ?>
+                        ?>
+
+
+
+
+                    </div>
+
+
+
 
 
 
 
                 </div>
-
-
-
-
-
-
-
+                <!-- /.container-fluid -->
+                <div class="pt-5"></div>
+                <div class="pt-5"></div>
+                <div class="pt-5"></div>
             </div>
-            <!-- /.container-fluid -->
+            <!-- End of Main Content -->
             <div class="pt-5"></div>
             <div class="pt-5"></div>
-            <div class="pt-5"></div>
-        </div>
-        <!-- End of Main Content -->
-        <div class="pt-5"></div>
-        <div class="pt-5"></div>
 
 
-        <?php include "include/footer.php"; ?>
+            <?php include "include/footer.php"; ?>
 
 
-        <?php include "include/scripts.php"; ?>
+            <?php include "include/scripts.php"; ?>
+
+        <?php
+
+    }
+    // if there is no user then re-direct to login page
+    else {
+        // show error message as alert
+        $_SESSION['error'] = "<div class='alert alert-danger' role='alert'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+            </button>
+            <strong><i class='fas fa-exclamation-circle'></i></strong> Please Login !
+            </div>";
+        header("Location: login.php");
+    }
+        ?>
