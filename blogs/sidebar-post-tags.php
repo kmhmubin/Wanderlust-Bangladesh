e49e740e8439be8c1175a39fa4ad9480d793e3d0
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-04-02 21:55:31 +0600

    POST TAGS LIST
    - display post tags for better readlity

--- blogs/sidebar-post-tags.php
+++ blogs/sidebar-post-tags.php
@@ -1,10 +1,21 @@
- <!-- Post tags list sidebar -->
- <div class="sidebar-box">
-     <h3 class="heading">Tags</h3>
-     <ul class="tags">
-         <li><a href="#">Travel</a></li>
-         <li><a href="#">Food</a></li>
-         <li><a href="#">Lifestyle</a></li>
-     </ul>
- </div>
- <!-- Post tags list sidebar end-->
+<!-- Post tags list sidebar -->
+<div class="sidebar-box">
+    <h3 class="heading">Tags</h3>
+    <ul class="tags">
+        <?php
+        $show_tags = "SELECT * FROM posts ORDER BY post_id DESC";
+        // query response
+        $result = mysqli_query($conn, $show_tags);
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+
+        ?>
+                <li><a href=""><?php echo $row['post_tags']; ?></a></li>
+        <?php
+
+            }
+        }
+        ?>
+    </ul>
+</div>
+<!-- Post tags list sidebar end-->
