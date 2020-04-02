88d309436b5eda768c66925b9321156ed8770639
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-04-02 21:37:13 +0600

    CATEGORIES LIST SHOW
    - list view of all the categories for the blog post

--- blogs/sidebar-categories-list.php
+++ blogs/sidebar-categories-list.php
@@ -1,10 +1,18 @@
-   <!-- Categories List sidebar start -->
-   <div class="sidebar-box">
-       <h3 class="heading">Categories</h3>
-       <ul class="categories">
-           <li><a href="#">Food <span>(12)</span></a></li>
-           <li><a href="#">Travel <span>(22)</span></a></li>
-           <li><a href="#">Culture <span>(37)</span></a></li>
-       </ul>
-   </div>
-   <!-- Categories List sidebar end -->
+  <!-- Categories List sidebar start -->
+  <div class="sidebar-box">
+      <h3 class="heading">Categories</h3>
+      <ul class="categories">
+
+          <?php
+            $category_grab = "SELECT * FROM categories";
+            $cat_response = mysqli_query($conn, $category_grab);
+            $span = 4;
+            while ($row = mysqli_fetch_assoc($cat_response)) {
+                $cat_title = $row['Category_Name'];
+                echo " <li><a href=''> $cat_title <span>($span) </span></a></li>";
+            }
+
+            ?>
+      </ul>
+  </div>
+  <!-- Categories List sidebar end -->
