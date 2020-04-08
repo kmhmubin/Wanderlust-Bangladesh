5cc22806b4fc96792bb397f0f2452dc3f7d1ebc5
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-04-08 16:36:55 +0600

    LATEST BLOG POST SHOWCASE
    - showing latest blog post on the index page

--- home/latest-blog.php
+++ home/latest-blog.php
@@ -12,49 +12,56 @@
         </div>
 
         <div class="row blog-entries">
+            <?php
+            $show_blogs = "SELECT * FROM posts WHERE post_status = 'published' ORDER BY post_id DESC LIMIT 3";
+            // query response
+            $result = mysqli_query($conn, $show_blogs);
 
-            <div class="col-md-4 main-content">
-                <a href="#" class="blog-entry element-animate" data-animate-effect="fadeIn">
-                    <img src="https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg" alt="Image placeholder">
-                    <div class="blog-content-body">
-                        <div class="post-meta">
-                            <span class="author mr-2"><img src="img/images/person_4.jpg" alt="Board Cat"> Board Cat</span>&bullet;
-                            <span class="mr-2">March 25, 2020 </span> &bullet;
-                            <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                        </div>
-                        <h2>How to Find the Video Games for Cats</h2>
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
+                    $post_category = $row['post_category'];
+                    $post_category_id = $row['post_category_id'];
+                    $post_author = $row['post_author'];
+                    $post_content = $row['post_content'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_views = $row['post_views'];
+                    $post_tags = $row['post_tags'];
+
+            ?>
+                    <div class="col-md-4 main-content">
+                        <a href="article.php?post=<?php echo $post_id; ?>" class="blog-entry element-animate" data-animate-effect="fadeIn">
+                            <img src="img/posts/<?php echo $post_image;  ?>" alt="Image placeholder">
+                            <div class="blog-content-body">
+                                <div class="post-meta">
+                                    <span class="author mr-2"> <?php echo $post_author; ?></span>&bullet;
+                                    <span class="mr-2"><?php echo $post_date;  ?> </span> &bullet;
+                                    <span class="ml-2"><span class="fa fa-comments"></span> <?php echo $post_comment_count;  ?></span>
+                                </div>
+                                <h2><?php echo $post_title; ?></h2>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
 
-            <div class="col-md-4 main-content">
-                <a href="#" class="blog-entry element-animate" data-animate-effect="fadeIn">
-                    <img src="https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg" alt="Image placeholder">
-                    <div class="blog-content-body">
-                        <div class="post-meta">
-                            <span class="author mr-2"><img src="img/images/person_4.jpg" alt="Board Cat"> Board Cat</span>&bullet;
-                            <span class="mr-2">March 25, 2020 </span> &bullet;
-                            <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                        </div>
-                        <h2>How to Find the Video Games for Cats</h2>
-                    </div>
-                </a>
-            </div>
 
-            <div class="col-md-4 main-content">
-                <a href="#" class="blog-entry element-animate" data-animate-effect="fadeIn">
-                    <img src="https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg" alt="Image placeholder">
-                    <div class="blog-content-body">
-                        <div class="post-meta">
-                            <span class="author mr-2"><img src="img/images/person_4.jpg" alt="Board Cat"> Board Cat</span>&bullet;
-                            <span class="mr-2">March 25, 2020 </span> &bullet;
-                            <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                        </div>
-                        <h2>How to Find the Video Games for Cats</h2>
-                    </div>
-                </a>
-            </div>
+            <?php
 
+                }
+            } else {
+                // show error message as alert
+                $_SESSION['error'] = "<div class='alert alert-danger' role='alert'>
+            <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+            </button>
+            <strong><i class='fas fa-exclamation-circle'></i></strong> Please Login !
+            </div>";
+                header("Location: login.php");
+            }
+            ?>
 
         </div>
 </section>
