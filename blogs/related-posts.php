4a964d519a3e2c325400e13b9a7bdf4059ce0e42
K.M.H.Mubin <52270073+kmhmubin@users.noreply.github.com>
2020-06-02 23:28:26 +0600

    fix and add the related post

--- blogs/related-posts.php
+++ blogs/related-posts.php
@@ -1,46 +1,30 @@
- <div class="row">
-     <!--    post box      -->
-     <div class="col-md-6 col-lg-4">
-         <a href="#" class="a-block sm d-flex align-items-center height-md" style="background-image: url('https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg'); ">
-             <div class="text">
-                 <div class="post-meta">
-                     <span class="category">Lifestyle</span>
-                     <span class="mr-2">March 25, 2020 </span> &bullet;
-                     <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                 </div>
-                 <h3>There’s a Cool New Way for Cat to Wear Socks and Sandals</h3>
-             </div>
-         </a>
-     </div>
-     <!--    post box end      -->
+    <?php
+    $show_blogs = "SELECT * FROM posts ORDER BY post_id DESC LIMIT 3";
+    // query response
+    $result = mysqli_query($conn, $show_blogs);
+    ?>
 
-     <!--    post box      -->
-     <div class="col-md-6 col-lg-4">
-         <a href="#" class="a-block sm d-flex align-items-center height-md" style="background-image: url('https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg'); ">
-             <div class="text">
-                 <div class="post-meta">
-                     <span class="category">Lifestyle</span>
-                     <span class="mr-2">March 25, 2020 </span> &bullet;
-                     <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                 </div>
-                 <h3>There’s a Cool New Way for Cat to Wear Socks and Sandals</h3>
-             </div>
-         </a>
-     </div>
-     <!--    post box end      -->
-
-     <!--    post box      -->
-     <div class="col-md-6 col-lg-4">
-         <a href="#" class="a-block sm d-flex align-items-center height-md" style="background-image: url('https://i.ibb.co/stmXFvC/dawid-zawila-zb2v-Ba-HYB2-I-unsplash.jpg'); ">
-             <div class="text">
-                 <div class="post-meta">
-                     <span class="category">Lifestyle</span>
-                     <span class="mr-2">March 25, 2020 </span> &bullet;
-                     <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
-                 </div>
-                 <h3>There’s a Cool New Way for Cat to Wear Socks and Sandals</h3>
-             </div>
-         </a>
-     </div>
-     <!--    post box end      -->
- </div>
+    <div class="row">
+        <?php
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+        ?>
+                <!--    post box      -->
+                <div class="col-md-6 col-lg-4">
+                    <a href="" article.php?post=<?php echo $row['post_id']; ?> class="a-block sm d-flex align-items-center height-md">
+                        <div class="text">
+                            <div class="post-meta">
+                                <!-- <span class="category">Lifestyle</span> -->
+                                <span class="mr-2"><?php echo $row['post_date']; ?> </span> &bullet;
+                                <!-- <span class="ml-2"><span class="fa fa-comments"></span> 3</span> -->
+                            </div>
+                            <h3><?php echo $row['post_title']; ?></h3>
+                        </div>
+                    </a>
+                </div>
+        <?php
+            }
+        }
+        ?>
+        <!--    post box end      -->
+    </div>
